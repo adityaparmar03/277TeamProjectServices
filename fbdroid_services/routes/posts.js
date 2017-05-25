9e7fcdec6dfa8daafa2a4a6761ecb79d9948e5c0
varshak <varsha.kankariya@sjsu.edu>
2017-05-25 15:53:04 -0700

    Updated code for sending notifications to the followers for a new post

--- fbdroid_services/routes/posts.js
+++ fbdroid_services/routes/posts.js
@@ -3,6 +3,8 @@ exports.addpost = function(req, res){
 	var emailid = req.body.emailid;
 	var content = req.body.content;
 	var media_url = req.body.media_url;
+	var screenname = req.body.screenname ;
+	var profile_pic = req.body.profile_pic ;
 	
 	var date = new Date();	
 	
@@ -20,7 +22,17 @@ exports.addpost = function(req, res){
 				if(!err){
 					console.log(results.modifiedCount);
 					if(results.modifiedCount == 1){
-						res.json({'status': '200', 'msg': 'Post added successfully'})
+						//Send the notifications to all the followers about the post
+						var data = {
+
+							'content' : 'New Post from '+ screenname ,
+							'time' : date ,
+							'profile_pic' : profile_pic, 
+							'screenname' : screenname  
+						}
+						sendNotificationsToFollowers ( emailid , data, res ) ;
+
+						//res.json({'status': '200', 'msg': 'Post added successfully'})
 					}
 					else{
 						res.json({'status': '400', 'msg': 'Error adding post'})
@@ -67,6 +79,60 @@ exports.getpost = function(req, res){
 			})
 		}
 	});
-	
-	
+}
+
+
+
+function sendNotificationsToFollowers( emailid , data , res  ) {
+
+	console.log("In posts.js : sendNotificationsToFollowers : Updating the notifications for all the followers!!!") ;
+
+	global.db.collection('fbdroid', function (err, collection) {
+		if(err){
+			console.log("No such collection exists" + err);		
+		}
+		else{
+
+			//var bulk = collection.initializeUnorderedBulkOp(); 
+			collection.findOne({"emailid" : emailid },{"_id": 0 , "following" : 1 } , function (err,result){
+				console.log("In posts.js : sendNotificationsToFollowers : Result after fetching the followers: + ", result)
+				if(err){
+
+					console.log("In posts.js : sendNotificationsToFollowers : Error while fetching the followers!!!");
+					throw err; 
+				}else{
+
+					console.log("In posts.js : sendNotificationsToFollowers : Result of the followers  ") ;
+					var followersList =  result.following ;
+
+					console.log("In posts.js : sendNotificationsToFollowers : Result after getting the array of followers: + ", followersList) ;
+					if(followersList.length > 0){
+
+						var bulk = collection.initializeUnorderedBulkOp();
+						bulk.find({'emailid' : {$in : followersList }})
+							.update({$addToSet : {"notifications" : data }}) ;
+						bulk.execute([],function(err, result ){
+
+							if(err){
+
+								console.log("Error while sending the notifications to the followers!!!") ;
+							}else{
+
+								console.log("Sent the notifications to the followers!!!") ;
+							}
+							
+							res.json({'status' : '200' ,'msg' : 'Posts added successfully!!!!'})
+						}) ;
+						
+
+					}else{
+
+						console.log("In posts.js : sendNotificationsToFollowers : No result returned for the followers!!!!") ;
+						res.json({'status' : '400' , 'msg' : 'Error while fetching the followers!!!'}) ;
+					}
+				}
+
+			} );
+		}
+	});
 }
